9427d1e91256b132e23f871dd9f7c4707c2f2753
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-03-30 15:04:49 +0700

    first commit

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+JMETERPLUGINSCMD=JMeterPluginsCMD.sh
+ 
+#./cleanup.sh
+ 
+if [ -d "./dashboard" ]; then rm -Rf ./dashboard; fi
+if [ -d "./reports" ]; then rm -Rf ./reports; fi
+rm -f results.jtl; 
+rm -f systhesis_results.csv;
---
+++ process_aggregate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ cat reports/aggregate_results.csv  |tr "," "|" | sed -e 's/^/|/' | sed -e 's/$/|\\\\n\\/' | sed -e '1 s/|/||/g'
