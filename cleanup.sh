12f9b1e431e7c1b685caab49161dbe98fdd9f482
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-03-30 15:29:21 +0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
- 
-JMETERPLUGINSCMD=JMeterPluginsCMD.sh
- 
+
 #./cleanup.sh
- 
+ICACLS  ./dashboard
+ICACLS  ./reports
 if [ -d "./dashboard" ]; then rm -Rf ./dashboard; fi
 if [ -d "./reports" ]; then rm -Rf ./reports; fi
 rm -f results.jtl; 
