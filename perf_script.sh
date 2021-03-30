bd63ab7f6fff1ac30b00e45dfe387627cc51e474
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-03-30 16:16:48 +0700

    Update perf_script.sh

--- perf_script.sh
+++ perf_script.sh
@@ -5,9 +5,14 @@ JMETERPLUGINSCMD=JMeterPluginsCMD.bat
 #./cleanup.sh
  
 # run jmeter and produce a JTL csv report
+echo "run jmeter and produce a JTL csv report"
 jmeter -H 10.225.3.1 -P 3128 -n -t  jpetstore_configurable_host.jmx -l results.jtl -e -o dashboard
- 
+
+
+
 # process JTL and covert it to a synthesis report as CSV
+echo "process JTL and covert it to a synthesis report as CSV"
+
 $JMETERPLUGINSCMD --generate-csv synthesis_results.csv --input-jtl results.jtl --plugin-type SynthesisReport
 $JMETERPLUGINSCMD --tool Reporter --generate-csv reports/aggregate_results.csv --input-jtl results.jtl --plugin-type AggregateReport
  
