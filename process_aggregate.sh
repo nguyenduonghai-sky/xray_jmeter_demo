c2aaeba01b1458cef560b2b18743875c295eba18
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-04-01 18:31:32 +0700

    Update process_aggregate.sh

--- process_aggregate.sh
+++ process_aggregate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat reports/aggregate_results.csv  |tr "," "|" | sed -e 's/^/|/' | sed -e 's/$/|\\\\n\\/' | sed -e '1 s/|/||/g'
+cat reports/aggregate_results.csv  |tr "," "|" | sed -e 's/$/|\\\\n\\/' | sed -e '1 s/|/||/g'
