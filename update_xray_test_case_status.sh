92448073b0646426ca1cbb0baa764108d7a3cb34
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-10-14 13:40:34 +0700

    update

--- update_xray_test_case_status.sh
+++ update_xray_test_case_status.sh
@@ -2,4 +2,4 @@
 curl --location --request POST 'https://xray.cloud.xpand-it.com/api/v1/graphql' \
 --header 'Authorization: Bearer {{TOKEN}}' \
 --header 'Content-Type: application/json' \
---data-raw '{"query":"mutation {\n    updateTestRunStatus( \n            id: \"{{TEST}}\",\n            status: \"PASSED\"\n        ) \n}","variables":{}}
+--data-raw '{"query":"mutation {\n    updateTestRunStatus( \n            id: \"{{TEST_EXECUTION_KEY}}\",\n            status: \"PASSED\"\n        ) \n}","variables":{}}
