c061f108b8918ad2bf1110e47d655c9f0ad16692
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-10-14 12:40:49 +0700

    update

--- create_xray_test_execution.sh
+++ create_xray_test_execution.sh
@@ -2,4 +2,4 @@
 curl --location --request POST 'https://xray.cloud.xpand-it.com/api/v1/graphql' \
 --header 'Authorization: Bearer {{TOKEN}}' \
 --header 'Content-Type: application/json' \
---data-raw '{"query":"mutation {\n    createTestExecution(\n        testIssueIds: []\n        jira: {\n            fields: {\n                summary: \"empty Test Execution\",\n                project: {key: \"XRAY\"} \n            }\n        }\n    ) {\n        testExecution {\n            issueId\n            jira(fields: [\"10008\"])\n        }\n        warnings\n    }\n}","variables":{}}'
+--data-raw '{"query":"mutation {\n    createTestExecution(\n        testIssueIds: []\n        jira: {\n            fields: {\n                summary: \"Test Execution of {{TEST}}, build time {{BUILD_TIME}}\",\n                project: {key: \"XRAY\"} \n            }\n        }\n    ) {\n        testExecution {\n            issueId\n            jira(fields: [\"10008\"])\n        }\n        warnings\n    }\n}","variables":{}}'
