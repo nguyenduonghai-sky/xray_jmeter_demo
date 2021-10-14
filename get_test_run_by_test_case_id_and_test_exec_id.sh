324b8429a73a554adb1f5a6193da5d6ac0010a2f
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-10-14 14:48:22 +0700

    Update

---
+++ get_test_run_by_test_case_id_and_test_exec_id.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl --location --request GET 'https://xray.cloud.xpand-it.com/api/v1/graphql' \
+--header 'Authorization: {{TOKEN}}' \
+--header 'Content-Type: application/json' \
+--data-raw '{"query":"{\n    getTestRun( testIssueId: \"{{TEST_CASE_ID}}\", testExecIssueId: \"{{TEST_EXECUTION_ID}}\") {\n        id\n        status {\n            name\n            color\n            description\n        }\n    }\n}","variables":{}}'
