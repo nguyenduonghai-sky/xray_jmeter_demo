ed03f07b86a3b937ed6c9492ae2b813538033a25
nguyenduonghai-sky <external.Hai.NguyenDuong@sky.de>
2021-10-14 14:01:33 +0700

    Update create_xray_test_execution.sh

--- create_xray_test_execution.sh
+++ create_xray_test_execution.sh
@@ -9,9 +9,6 @@ curl --location --request POST 'https://xray.cloud.xpand-it.com/api/v1/import/ex
     "tests" : [
         {
             "testKey" : "{{TEST}}",
-            "start" : "2014-08-30T11:47:35+01:00",
-            "finish" : "2014-08-30T11:50:56+01:00",
-            "comment" : "Successful execution",
             "status" : "PASS"
         }
  ]
